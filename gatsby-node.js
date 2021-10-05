b4970a2aa81f29d41372e2f0e0e205c13d1189ab
unknown <vinceoldmark@gmail.com>
2021-10-05 07:47:15 +0100

    wut\as

--- gatsby-node.js
+++ gatsby-node.js
@@ -44,28 +44,24 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
           }
         }
       }
-     
     }
   `)
 
-
   const resulto = await graphql(`
-  {
-  
-    articles {
+    {
       articles {
-        author
-        title
-        description
-        url
-        urlToImage
-        publishedAt
-        content
+        articles {
+          author
+          title
+          description
+          url
+          urlToImage
+          publishedAt
+          content
+        }
       }
-    }   
-   
-  }
-`)
+    }
+  `)
   // Handle errors
   if (result.errors) {
     reporter.panicOnBuild(`Error while running GraphQL query.`)
@@ -84,27 +80,19 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     })
   })
 
+  resulto.data.articles.articles.forEach(
+    ({ author, title, description, url, urlToImage, publishedAt, content }) => {
+      const urla = new URL(url)
+      const rel = urla.toString().substring(url.origin.length)
+      createPage({
+        path: rel,
+        component: notiPage,
+        context: {
+          data: description,
+        },
+      })
+    }
+  )
 
  
-
-  resulto.data.articles.articles.forEach(({
-    author,
-    title,
-    description,
-    url,
-    urlToImage,
-    publishedAt,
-    content }) => {
-    const urla = new URL(url)
-    const rel = urla.toString().substring(url.origin.length)
-    createPage({
-      path: rel,
-      component: notiPage,
-      context: {
-        data: description,
-      },
-    })
-  })
- 
-  // Create category pages
 }
