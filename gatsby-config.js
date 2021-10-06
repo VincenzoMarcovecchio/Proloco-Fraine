f3dce67aa43f8cce68179feca00ee64ab0c675be
unknown <vinceoldmark@gmail.com>
2021-10-06 15:08:59 +0100

    major changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,12 +23,7 @@ module.exports = {
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-json`,
-    {
-      resolve: `gatsby-plugin-layout`,
-      options: {
-        component: require.resolve(`./src/components/layout.js`),
-      },
-    },
+
     "gatsby-plugin-loadable-components-ssr",
     {
       resolve: "gatsby-plugin-next-seo",
@@ -234,6 +229,12 @@ module.exports = {
             title: "Proloco Fraine RSS Feed",
           },
         ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/components/layout.js`),
       },
     },
     {
