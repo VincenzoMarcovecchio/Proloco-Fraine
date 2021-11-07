6b22286984b3d0d1444d00b5a6f62097834d147f
unknown <vinceoldmark@gmail.com>
2021-11-07 20:00:44 +0000

    change

---
+++ src/templates/lastTemplate.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Container from "@material-ui/core/Container"
+
+export default function Ciccccc({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+ console.log(data)
+  return (
+    <React.Fragment>
+      
+      <Container maxWidth="sm">
+        <article className="blog-post">
+          ciao
+        </article>
+
+        
+      </Container>
+    </React.Fragment>
+  )
+}
+
+
