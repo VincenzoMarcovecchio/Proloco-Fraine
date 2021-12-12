b3c7d3455c73333b5bd10f759774e1032ff9576a
unknown <vinceoldmark@gmail.com>
2021-12-12 18:53:33 +0000

    fukka

--- gatsby-node.js
+++ gatsby-node.js
@@ -129,8 +129,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   }
 
   await strutto.forEach(async dis => {
-    let patto = await dis[Object.keys(dis)[2]].replace(/\W+/g, "-").toLowerCase()
-
+    let patto = await dis[Object.keys(dis)[1]].replace(/\W+/g, "-")
     createPage({
       path: `/${patto}/`,
       component: struttoTemplate,
