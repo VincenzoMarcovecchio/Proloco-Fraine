359e02842229f35e5ae14ab54313166f86b9e996
unknown <vinceoldmark@gmail.com>
2021-12-12 18:38:39 +0000

    fukka

--- src/templates/struttoTemplate.js
+++ src/templates/struttoTemplate.js
@@ -1,8 +1,78 @@
 import React from "react"
+import Container from "@material-ui/core/Container"
+import { GatsbySeo } from "gatsby-plugin-next-seo"
 
 const Strutto = ({ pageContext }) => {
   console.log(pageContext)
-  return <h1>ciao</h1>
+
+  let strutte = pageContext.data
+  let title = strutte[Object.keys(strutte)[2]]
+    .replace(/\W+/g, "-")
+    .toLowerCase()
+  let stelle = strutte[Object.keys(strutte)[3]]
+  let where = strutte[Object.keys(strutte)[5]]
+    .replace(/\W+/g, "-")
+    .toLowerCase()
+  let tel = strutte[Object.keys(strutte)[6]]
+  let indi = strutte[Object.keys(strutte)[8]]
+  let provi = strutte[Object.keys(strutte)[4]]
+  let posco = strutte[Object.keys(strutte)[7]]
+  let tipostrutta = strutte[Object.keys(strutte)[1]].toLowerCase()
+
+  return (
+    <>
+      <LocalBusinessJsonLd
+        type={`${tipostrutta}`}
+        id={`https://www.prolocofraine.org/${title}`}
+        name={`${title}`}
+        description={`${title} - ${where} - ${stelle} 
+  `}
+        url={`https://www.prolocofraine.org/${title}`}
+        telephone={`${tel}`}
+        address={{
+          streetAddress: `${indi}`,
+          addressLocality: `${where}`,
+          addressRegion: `${provi}`,
+          postalCode: `${posco}`,
+          addressCountry: "IT",
+        }}
+        geo={{
+          latitude: "",
+          longitude: "",
+        }}
+        images={["https://prolocofraine.org/images/nocche-fritte"]}
+      />
+      <GatsbySeo
+        title={title}
+        description={`${title} - ${where} - ${stelle} 
+  `}
+        canonical={`https://www.prolocofraine.org/${title}`}
+        openGraph={{
+          url: `https://www.prolocofraine.org/${title}`,
+          title: `${title}`,
+          description: `${title} - ${where} - ${stelle} `,
+          images: [
+            {
+              url: ``,
+              width: 800,
+              height: 600,
+              alt: "proloco fraine",
+            },
+          ],
+          site_name: "Proloco Fraine",
+        }}
+        twitter={{
+          handle: "Vincenzo Marcovecchio",
+          site: "Proloco Fraine",
+          cardType: "summary_large_image",
+        }}
+      />
+
+      <Container style={{ marginTop: "3rem" }} maxWidth="sm">
+        <h1>{title}</h1>
+      </Container>
+    </>
+  )
 }
 
 export default Strutto
