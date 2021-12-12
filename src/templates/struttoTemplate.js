eacf28bf824377ed7f1b740324b25df689d302ca
unknown <vinceoldmark@gmail.com>
2021-12-12 17:14:10 +0000

    fukka

--- src/templates/struttoTemplate.js
+++ src/templates/struttoTemplate.js
@@ -1,6 +1,8 @@
 import React from "react"
 
-export const Strutto = ({ pageContext }) => {
+const Strutto = ({ pageContext }) => {
   console.log(pageContext)
-  return(<h1>ciao</h1>)
+  return <h1>ciao</h1>
 }
+
+export default Strutto
