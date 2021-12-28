4639ab0370659d62bfbc49db57872ad5bdca8e50
unknown <vinceoldmark@gmail.com>
2021-12-28 23:24:07 +0000

    pussy farting3

--- gatsby-node.js
+++ gatsby-node.js
@@ -208,12 +208,13 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     let susa = await fetch(
       `https://sheltered-meadow-66603.herokuapp.com/noti/${luca}`
     )
+    const lola = await susa.json()
 
     await createPage({
       path: `/${luca}/`,
       component: abruNews,
       context: {
-        data: susa.results,
+        data: lola.results,
       },
     })
   })
