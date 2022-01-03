2f8de61f85f071d9f68d0b6b44bf396628c7bd80
unknown <vinceoldmark@gmail.com>
2022-01-03 19:29:49 +0000

    ciao

--- gatsby-node.js
+++ gatsby-node.js
@@ -224,7 +224,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
         let rollot = await fetch(
           `https://sheltered-meadow-66603.herokuapp.com/noti/${luca}`
         )
-        let gigi = await JSON.stringify(rollot)
+        let gigi = await JSON.stringify(rollot.json())
 
         await createPage({
           path: `/${luca}`,
