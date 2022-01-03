400e601c7ad9f0d427f67d889f919afb95971b17
unknown <vinceoldmark@gmail.com>
2022-01-03 17:03:49 +0000

    ciao

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,16 +5,13 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React, { useState } from "react"
+import React  from "react"
 import PropTypes from "prop-types"
-import AuthOverlay from "./AuthOverlay"
 import Header from "./header"
 import "./layout.css"
 import Footer from "../components/footer"
 import Cookie from "../components/cookie/Cookie"
 import { Helmet } from "react-helmet"
-import { useIdentityContext } from "react-netlify-identity-gotrue"
-import { Link } from "gatsby"
 
 const Layout = ({ children }) => {
   return (
@@ -24,11 +21,7 @@ const Layout = ({ children }) => {
           lang: "it",
         }}
       >
-        <script
-          data-ad-client="ca-pub-7565213898571907"
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        ></script>
+     
       </Helmet>
 
       <Header />
