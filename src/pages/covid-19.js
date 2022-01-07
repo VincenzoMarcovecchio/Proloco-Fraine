e2ca43df14ee406a8cf50d8751b6d4c5b8ccf27b
unknown <vinceoldmark@gmail.com>
2022-01-07 19:39:04 +0000

    ciao

--- src/pages/covid-19.js
+++ src/pages/covid-19.js
@@ -1,16 +1,8 @@
 import React from "react"
 import Container from "@material-ui/core/Container"
 import { makeStyles } from "@material-ui/core/styles"
-import Divider from "@material-ui/core/Divider"
 import Grid from "@material-ui/core/Grid"
-import Card from "@material-ui/core/Card"
-import CardActions from "@material-ui/core/CardActions"
-import CardContent from "@material-ui/core/CardContent"
-import Button from "@material-ui/core/Button"
-import Typography from "@material-ui/core/Typography"
-import CircularProgress from "@material-ui/core/CircularProgress"
-import { graphql } from "gatsby"
-import Img from "gatsby-image"
+
 import corona from "../../src/images/covidimageinformationalreport.png"
 const useStyles = makeStyles(theme => ({
   container: {
