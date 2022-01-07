f08251d8ebe31923373544bc4023c4318f7c7cbd
unknown <vinceoldmark@gmail.com>
2022-01-07 13:24:19 +0000

    ciao

--- src/templates/abruNews.js
+++ src/templates/abruNews.js
@@ -5,12 +5,25 @@ import Typography from "@material-ui/core/Typography"
 
 const Abruzzio = ({ pageContext }) => {
   console.log(pageContext)
+  useEffect(() => {
+    var divs = document.querySelectorAll("img")
+    var divse = (document.querySelectorAll(".group-right").style.css =
+      "display: none")
+    var divss = (document.querySelectorAll(".leaflet-layer").style.css =
+      "display: none")
 
+    for (let element of divs) {
+      let sorca = await element.src
+      let gigi = await sorca.replace(window.location.origin, "")
+      element.src = `https://abruzzoturismo.it${gigi}`
+      idx.href = `https://abruzzoturismo`
+    }
+  })
   return (
     <>
       <Container style={{ marginTop: "3rem" }} maxWidth="sm">
         <article className="blog-post">
-        <div
+          <div
             className="blog-post-content"
             dangerouslySetInnerHTML={{ __html: pageContext.data.results.title }}
           />
