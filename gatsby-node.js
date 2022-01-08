b988a688f24f57a53054dba7383b1eb761e1d104
unknown <vinceoldmark@gmail.com>
2022-01-08 11:31:27 +0000

    ciao

--- gatsby-node.js
+++ gatsby-node.js
@@ -171,36 +171,36 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     console.log(e)
   }
 
-  try {
-    let getJSON = uri => fetch(uri).then(response => response.json())
+  // try {
+  //   let getJSON = uri => fetch(uri).then(response => response.json())
 
-    const roof = await getJSON(
-      `https://newsdata.io/api/1/news?apikey=pub_27444837fea2a2e2cc240d2e4d3dcab923c4&q=covid&page=1`
-    )
+  //   const roof = await getJSON(
+  //     `https://newsdata.io/api/1/news?apikey=pub_27444837fea2a2e2cc240d2e4d3dcab923c4&q=covid&page=1`
+  //   )
 
-    await roof.results.forEach(async kok => {
-      let luca = await kok.title.replace(/\s+/g, "-").toLowerCase()
-      let fabio = await luca.replace(/\?/g, "-")
-      let lore = await fabio.replace(/\%/g, "-")
-      let fern = await lore.replace(/\“/g, "-")
-      let koka = await fern.replace(/\”/g, "-")
-      let popo = await koka.replace(/\'/g, "-")
-      let holo = await popo.replace(/\,/g, "-")
-      let fuffo = await holo.replace(/\:/g, "-")
-      let fuffa = await fuffo.replace(/\’/g, "-")
-      let fuffat = await fuffa.replace(/\./g, "-")
+  //   await roof.results.forEach(async kok => {
+  //     let luca = await kok.title.replace(/\s+/g, "-").toLowerCase()
+  //     let fabio = await luca.replace(/\?/g, "-")
+  //     let lore = await fabio.replace(/\%/g, "-")
+  //     let fern = await lore.replace(/\“/g, "-")
+  //     let koka = await fern.replace(/\”/g, "-")
+  //     let popo = await koka.replace(/\'/g, "-")
+  //     let holo = await popo.replace(/\,/g, "-")
+  //     let fuffo = await holo.replace(/\:/g, "-")
+  //     let fuffa = await fuffo.replace(/\’/g, "-")
+  //     let fuffat = await fuffa.replace(/\./g, "-")
 
-      await createPage({
-        path: `/${fuffat}/`,
-        component: nuoveNews,
-        context: {
-          data: kok,
-        },
-      })
-    })
-  } catch (e) {
-    console.log(e)
-  }
+  //     await createPage({
+  //       path: `/${fuffat}/`,
+  //       component: nuoveNews,
+  //       context: {
+  //         data: kok,
+  //       },
+  //     })
+  //   })
+  // } catch (e) {
+  //   console.log(e)
+  // }
 
   
   try {
