f072ac3e83db07c6d66e82747d0aa98b54bc5f80
unknown <vinceoldmark@gmail.com>
2022-01-08 09:46:09 +0000

    ciao

--- src/components/Eventi.js
+++ src/components/Eventi.js
@@ -2,30 +2,30 @@ import React from "react"
 import { graphql } from "gatsby"
 import Container from "@material-ui/core/Container"
 
-// const NewComponent = data => {
-//   return (
-//     <Container style={{ marginTop: "3rem" }} maxWidth="sm">
-//       {data.data.pollodocs !== undefined ? (
-//         <div
-//           dangerouslySetInnerHTML={{
-//             __html: data.data.pollodocs.results.title,
-//           }}
-//         ></div>
-//       ) : (
-//         "caricamento in corso"
-//       )}
-//     </Container>
-//   )
-// }
+const NewComponent = data => {
+  return (
+    <Container style={{ marginTop: "3rem" }} maxWidth="sm">
+      {data.data.pollodocs !== undefined ? (
+        <div
+          dangerouslySetInnerHTML={{
+            __html: data.data.pollodocs.results.title,
+          }}
+        ></div>
+      ) : (
+        "caricamento in corso"
+      )}
+    </Container>
+  )
+}
 
-// export const cazzoDio = graphql`
-//   query gallo {
-//     pollodocs {
-//       results {
-//         title
-//       }
-//     }
-//   }
-// `
+export const cazzoDio = graphql`
+  query gallo {
+    pollodocs {
+      results {
+        title
+      }
+    }
+  }
+`
 
-// export default NewComponent
+export default NewComponent
