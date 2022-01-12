17841dc29eacaa2fed0fd5f091d459c931c64024
unknown <vinceoldmark@gmail.com>
2022-01-12 20:48:09 +0000

    change

--- gatsby-node.js
+++ gatsby-node.js
@@ -184,7 +184,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     let getJSON = uri => fetch(uri).then(response => response.json())
 
     const roof = await getJSON(
-      `https://newsdata.io/api/1/news?apikey=pub_27444837fea2a2e2cc240d2e4d3dcab923c4&q=covid&page=1`
+      `https://newsdata.io/api/1/news?apikey=pub_27444837fea2a2e2cc240d2e4d3dcab923c4&q=covid`
     )
 
     await roof.results.forEach(async kok => {
@@ -204,34 +204,34 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   }
 
   
-  // try {
-  //   let getJSON = uri => fetch(uri).then(response => response.json())
+  try {
+    let getJSON = uri => fetch(uri).then(response => response.json())
 
-  //   const roof = await getJSON(
-  //     `https://newsdata.io/api/1/news?apikey=pub_34650b5bc097af8addc0ee5589e2d8fe711f&q=natura`
-  //   )
+    const roof = await getJSON(
+      `https://newsdata.io/api/1/news?apikey=pub_34650b5bc097af8addc0ee5589e2d8fe711f&q=natura&page=1`
+    )
 
-  //   await roof.results.forEach(async kok => {
-  //     let luca = await kok.link.substring(30)
+    await roof.results.forEach(async kok => {
+      let luca = await kok.link.substring(30)
      
 
-  //     await createPage({
-  //       path: `/${luca}/`,
-  //       component: nuoveNews,
-  //       context: {
-  //         data: kok,
-  //       },
-  //     })
-  //   })
-  // } catch (e) {
-  //   console.log(e)
-  // }
+      await createPage({
+        path: `/${luca}/`,
+        component: nuoveNews,
+        context: {
+          data: kok,
+        },
+      })
+    })
+  } catch (e) {
+    console.log(e)
+  }
 
   try {
     let getJSON = uri => fetch(uri).then(response => response.json())
 
     const cane = await getJSON(
-      `https://newsdata.io/api/1/news?apikey=pub_27444837fea2a2e2cc240d2e4d3dcab923c4&q=cucina`
+      `https://newsdata.io/api/1/news?apikey=pub_27444837fea2a2e2cc240d2e4d3dcab923c4&q=cucina&page=1`
     )
 
     await cane.results.forEach(async kok => {
